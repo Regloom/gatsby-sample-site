4b11714f020997eb2ea9137a55624208b6b64b14
Egor Tokarev <egor.tokarev@outlook.com>
2023-06-19 12:11:01 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby sample site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Gatsby Site Title`,
+    siteUrl: `https://localhost:8000`,
+    description: `My First Gatsby Site`,
   },
   plugins: [    
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(item => (
+          <li key={item.name}>
+            {item.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
